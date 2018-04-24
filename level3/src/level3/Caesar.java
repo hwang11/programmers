1f866f8a023843a016f4277739b40a45bbc05bc1
lovehero0206 <tjsdk2769@naver.com>
2018-04-24 21:05:23 +0900

    level3 시저암호 java

---
+++ level3/src/level3/Caesar.java
@@ -0,0 +1,33 @@
+package level3;
+
+import java.util.*;
+public class Caesar {
+	String caesar(String s, int n) {
+		String result = "";
+		// 함수를 완성하세요.
+		int a,k;
+		a = k = 0;
+    //n %= 26;
+	    for(int i=0; i<s.length();i++){
+	      k = (int)s.charAt(i);
+	      a = k + n;
+        if(s.charAt(i) == ' '){
+        	result+= " ";
+        }
+	      else if(k>=65 && k<=90) {
+	    	  if(a>90)
+	        	result += (char)((65+(a-91)%26));
+        }
+	      else if(k>=97 && k<=122){
+          if(a>122)
+	        	result += (char)((97+(a-123)%26));
+        }
+      }
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Caesar c = new Caesar();
+		System.out.println("s는 'a B z', n은 4인 경우: " + c.caesar("a B z", 30));
+	}
+}

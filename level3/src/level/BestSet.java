974194e524e201736280dd50cfeddec2842fdacc
lovehero0206 <tjsdk2769@naver.com>
2018-04-30 21:29:26 +0900

    level4 최고의 집합

--- level3/src/level/BestSet.java
+++ level3/src/level/BestSet.java
@@ -21,7 +21,6 @@ public class BestSet {
 		    }
 	    public static void main(String[] args) {
 	        BestSet c = new BestSet();
-	        //아래는 테스트로 출력해 보기 위한 코드입니다.
 	        System.out.println(Arrays.toString(c.bestSet(3,13)));
 	    }
 
